374f51641293ff9f012788d310c7e8451d34c605
Catgirl <zeroaninea@gmail.com>
2025-09-06 13:02:36 +0400

    hello user added

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Inject, Get } from '@nestjs/common';
+import { Controller, Inject, Get, Param } from '@nestjs/common';
 import { HelloService } from './hello.service';
 
 @Controller('hello')
@@ -15,7 +15,7 @@ export class HelloController {
   }
 
   @Get('user/:name')
-  getHelloWithName(@Inject('name') name: string): string {
+  getHelloWithName(@Param('name') name: string): string {
     return this.helloService.getHelloWithName(name);
   }
 }
