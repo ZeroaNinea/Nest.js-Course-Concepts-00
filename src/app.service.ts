2c72667c89c959199b753c9e7bfaa5667691a42f
Catgirl <zeroaninea@gmail.com>
2025-09-06 20:55:55 +0400

    log the app name

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(@Inject() private readonly configService: ConfigService) {}
+
   getHello(): string {
+    const appName = this.configService.get<string>('APP_NAME', 'defaultValue');
+    console.log(appName, 'appName');
     return 'Hello World!';
   }
 }
