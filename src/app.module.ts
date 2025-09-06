0823cb42f681d84ec72a8fa28d3f63acfd342ea3
Catgirl <zeroaninea@gmail.com>
2025-09-06 22:33:18 +0400

    app.config.ts created

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HelloModule } from './hello/hello.module';
 import { UserModule } from './user/user.module';
+// import appConfig from './config/app.config';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { UserModule } from './user/user.module';
       validationSchema: Joi.object({
         APP_NAME: Joi.string().default('defaultApp'),
       }),
+      // load: [appConfig], // Use the `appConfig` for default values.
     }),
     HelloModule,
     UserModule,
