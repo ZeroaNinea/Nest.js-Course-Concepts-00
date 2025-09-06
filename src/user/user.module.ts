354b977cae92769c8c29af10b647da166e3c7c86
Catgirl <zeroaninea@gmail.com>
2025-09-06 14:51:16 +0400

    HelloService injected into the UserService

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Inject, Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { HelloService } from 'src/hello/hello.service';
 
 @Module({
   controllers: [UserController],
   providers: [UserService],
 })
-export class UserModule {}
+export class UserModule {
+  constructor(
+    @Inject(HelloService) private readonly userService: HelloService,
+  ) {}
+}
