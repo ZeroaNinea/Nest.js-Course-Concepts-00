9888650ccba2ca579f6d8ab022a3b1f877f9ddb5
Catgirl <zeroaninea@gmail.com>
2025-09-06 20:22:00 +0400

    comment added

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true,
+      isGlobal: true, // Import ConfigModule globally to use environment variables.
     }),
     HelloModule,
     UserModule,
