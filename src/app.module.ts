e9f0e0ccbd54c1c4f3c295afc6b60417d6df9e4b
Catgirl <zeroaninea@gmail.com>
2025-09-06 21:47:31 +0400

    set default value with joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,15 @@ import { AppService } from './app.service';
 import { HelloModule } from './hello/hello.module';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import * as joi from 'joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // Import ConfigModule globally to use environment variables.
+      validationSchema: joi.object({
+        APP_NAME: joi.string().default('defaultApp'),
+      }),
     }),
     HelloModule,
     UserModule,
